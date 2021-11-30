2ff82e9ec1a0e08346a95d5bfc17807881d8fdb8
Daniela510 <43034761+Daniela510@users.noreply.github.com>
2021-11-30 19:53:07 +0100

    tally and 5 rounds
    Finisheeeed

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log('Hi there!');
 
 const outcomes = ['Scissors','Paper','Rock'];
-
+let p = 0;
+let c = 0;
+let t = 0;
 
 
 function computerPlay() {
@@ -14,24 +16,30 @@ function playRound(playerSelection, computerSelection) {
 if (playerSelection == 'Rock') {
   switch (computerSelection) {
     case 'Scissors':
+      p++;
       return `You win! ${playerSelection} beats ${computerSelection}!`;
       break;
     case 'Paper':
+      c++;
       return `You lose! ${computerSelection} beats ${playerSelection}!` ;
       break;
     case 'Rock':
+      t++;
       return `You tie! ${computerSelection} is ${playerSelection}`;
       break;
   }
 } else if (playerSelection == 'Paper') {
   switch (computerSelection) {
     case 'Scissors':
+      c++;
       return `You lose! ${computerSelection} beats ${playerSelection}!`;
       break;
     case 'Paper':
+      t++;
       return `You tie! ${computerSelection} is ${playerSelection}`;
       break;
     case 'Rock':
+      p++;
       return `You win! ${playerSelection} beats ${computerSelection}!`;
       break;
   }
@@ -39,12 +47,15 @@ if (playerSelection == 'Rock') {
 } else if (playerSelection == 'Scissors'){
   switch (computerSelection) {
     case 'Scissors':
+      t++;
       return `You tie! ${computerSelection} is ${playerSelection}`;
       break;
     case 'Paper':
+      p++;
       return `You win! ${playerSelection} beats ${computerSelection}!`;
       break;
     case 'Rock':
+      c++;
       return `You lose! ${computerSelection} beats ${playerSelection}!`;
       break;
   }
@@ -56,16 +67,31 @@ if (playerSelection == 'Rock') {
 }
 
 
-const computerSelection = computerPlay();
-const input = prompt("Rock, paper or scissors?");
-
-for (let i = 0; i < outcomes.length; i++) {
-  if(input.toUpperCase() === outcomes[i].toUpperCase()) {
-playerSelection = outcomes[i];}
+function game() {
+  for (var i = 0; i < 5; i++) {
+    // get computer choice
+    const computerSelection = computerPlay();
+    // get pley choice
+    const input = prompt("Rock, paper or scissors?");
+    // make it case insesnsitive
+    for (let i = 0; i < outcomes.length; i++) {
+      if(input.toUpperCase() === outcomes[i].toUpperCase()) {
+    playerSelection = outcomes[i];}
+    }
+    playRound(playerSelection, computerSelection);
+  }
+  console.log(`The human won ${p} rounds!`);
+  console.log(`The computer won ${c} rounds!`);
+  console.log(`${t} ties.`);
+  if (p>c) {
+    console.log('HUZZA! YOU WON!');
+  } else if (c<p) {
+    console.log('Ha, the computer wins this game. You can always try again...');
+  } else {
+    console.log('No winners or losers this time, refresh?');
+  }
 }
-
-
- console.log(playRound(playerSelection, computerSelection));
+game();
 
 
 
